fb7a8348f8750ed5abc047b20e1d66cd545ae6d6
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-03-15 15:43:29 -0400

    Title window "BlockGame"

--- src/BlockGame.cpp
+++ src/BlockGame.cpp
@@ -30,7 +30,7 @@ void display()
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);                 // Initialize GLUT
-  glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+  glutCreateWindow("BlockGame"); // Create a window with the given title
   glutInitWindowSize(320, 320);          // Set the window's initial width & height
   glutInitWindowPosition(50, 50);        // Position the window's initial top-left corner
   glutDisplayFunc(display);              // Register display callback handler for window re-paint
