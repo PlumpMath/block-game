2aa2860ac3723005feee2f7588a80885ea0ed108
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-03-23 10:57:26 -0400

    Use block_game namespace

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,52 @@
 
 #include "freeglut/glut.h"
 
-float angle = 0;
-
-void Display()
+namespace block_game
 {
-  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
+  float angle = 0;
 
-  glBegin(GL_QUADS);
-  glColor3f(1.0f, 0.0f, 0.0f);
-  static const float points[][2] = {{-0.5f, -0.5f}, {0.5f, -0.5f}, {0.5f, 0.5f}, {-0.5f, 0.5f}};
-  for (const float* point : points)
+  void Display()
   {
-    glVertex2f(point[0] * cos(angle) - point[1] * sin(angle), point[0] * sin(angle) + point[1] * cos(angle));
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    static const float points[][2] = {{-0.5f, -0.5f}, {0.5f, -0.5f}, {0.5f, 0.5f}, {-0.5f, 0.5f}};
+    for (const float* point : points)
+    {
+      glVertex2f(point[0] * cos(angle) - point[1] * sin(angle), point[0] * sin(angle) + point[1] * cos(angle));
+    }
+    glEnd();
+
+    glFlush();
   }
-  glEnd();
 
-  glFlush();
-}
+  void Update()
+  {
+    angle += 0.01f;
+    glutPostRedisplay();
+  }
 
-void Update()
-{
-  angle += 0.01f;
-  glutPostRedisplay();
-}
+  void StartTimer();
 
-void StartTimer();
+  void Timer(const int value)
+  {
+    StartTimer();
+    Update();
+  }
 
-void Timer(const int value)
-{
-  StartTimer();
-  Update();
-}
-
-void StartTimer()
-{
-  glutTimerFunc(1000 / 60, Timer, 0);
+  void StartTimer()
+  {
+    glutTimerFunc(1000 / 60, Timer, 0);
+  }
 }
 
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
   glutCreateWindow("BlockGame");
-  glutDisplayFunc(Display);
-  StartTimer();
+  glutDisplayFunc(block_game::Display);
+  block_game::StartTimer();
   glutMainLoop();
 }
