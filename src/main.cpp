1a16c2fa19b2a8a19b4e4a6f7f6af50faf725d7c
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-03-27 17:12:59 -0400

    Use GLSL shaders when rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "glew/glew.h"
 #include "glfw/glfw3.h"
 
 #include "game/world.h"
@@ -18,6 +19,8 @@ int main()
 
   glfwMakeContextCurrent(window);
 
+  glewInit();
+
   double time = 0;
   double new_time;
   double delta;
@@ -26,6 +29,47 @@ int main()
   int height;
 
   block_game::World world;
+
+  const char* vertex_source = R"(
+#version 110
+
+attribute vec4 in_Position;
+
+varying vec4 pass_Color;
+
+void main(void)
+{
+	gl_Position = in_Position;
+
+	pass_Color = gl_Color;
+}
+  )";
+
+  const char* fragment_source = R"(
+#version 110
+
+varying vec4 pass_Color;
+
+void main(void)
+{
+	gl_FragColor = pass_Color;
+}
+  )";
+
+  const int vertex_id = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_id, 1, &vertex_source, nullptr);
+  glCompileShader(vertex_id);
+
+  const int fragment_id = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_id, 1, &fragment_source, nullptr);
+  glCompileShader(fragment_id);
+
+  const int program_id = glCreateProgram();
+  glAttachShader(program_id, vertex_id);
+  glAttachShader(program_id, fragment_id);
+
+  glLinkProgram(program_id);
+  glValidateProgram(program_id);
 
   while (!glfwWindowShouldClose(window))
   {
@@ -36,11 +80,24 @@ int main()
 
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
+
+    glUseProgram(program_id);
     world.Display();
+    glUseProgram(0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+
+
+  glDetachShader(program_id, vertex_id);
+  glDetachShader(program_id, fragment_id);
+
+  glDeleteShader(vertex_id);
+  glDeleteShader(fragment_id);
+
+  glDeleteProgram(program_id);
 
   glfwDestroyWindow(window);
   glfwTerminate();
