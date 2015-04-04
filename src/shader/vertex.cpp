2c1bd6aa2416fad88c340f5fd47cf8a8d06fc162
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-04-03 22:04:53 -0400

    Replace in_Position with gl_Vertex.xyz

--- src/shader/vertex.cpp
+++ src/shader/vertex.cpp
@@ -9,13 +9,11 @@ uniform vec3 color;
 uniform vec3 position;
 uniform mat3 rotation;
 
-attribute vec3 in_Position;
-
 varying vec3 pass_Color;
 
 void main()
 {
-  gl_Position = vec4(position + rotation * (radius * in_Position), 1.0);
+  gl_Position = vec4(position + rotation * (radius * gl_Vertex.xyz), 1.0);
   pass_Color = color * -(rotation * gl_Normal).zzz;
 }
 
