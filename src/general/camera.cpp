70c09fedcbe4703adc8874bfe2a871d093df8272
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-04-21 12:12:41 -0400

    Add Camera fields and constructor

---
+++ src/general/camera.cpp
@@ -0,0 +1,7 @@
+#include "general/camera.h"
+
+namespace block_game
+{
+  Camera::Camera() : field_of_view_{0.0F}, z_near_{0.0F}, z_far_{0.0F}, aspect_ratio_{0.0F}
+  {}
+}
--- src/general/camera.h
+++ src/general/camera.h
@@ -1,10 +1,24 @@
 #ifndef BLOCK_GAME_GENERAL_CAMERA_H_
 #define BLOCK_GAME_GENERAL_CAMERA_H_
 
+#include "general/vector_3f.h"
+
 namespace block_game
 {
   class Camera
-  {};
+  {
+  public:
+    Camera();
+
+  private:
+    Vector3F position_;
+    Vector3F rotation_;
+
+    float field_of_view_;
+    float z_near_;
+    float z_far_;
+    float aspect_ratio_;
+  };
 }
 
 #endif
