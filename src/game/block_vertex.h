e5c9b54d07e2ed334562ac19b7d81693ed016806
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-04-26 06:59:00 -0400

    Add BlockVertex members

--- src/game/block_vertex.h
+++ src/game/block_vertex.h
@@ -1,10 +1,15 @@
 #ifndef BLOCK_GAME_GAME_BLOCK_VERTEX_H_
 #define BLOCK_GAME_GAME_BLOCK_VERTEX_H_
 
+#include "general/vector_3f.h"
+
 namespace block_game
 {
   struct BlockVertex
-  {};
+  {
+    Vector3F position;
+    Vector3F normal;
+  };
 }
 
 #endif
