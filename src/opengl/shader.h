24d3b72f6f143133c21f80476736b70cbac6a617
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-05-06 21:08:13 -0400

    Refactor Shader class

--- src/opengl/shader.h
+++ src/opengl/shader.h
@@ -10,19 +10,21 @@ namespace block_game
   class Shader
   {
   public:
-    Shader(const GLenum, const std::string);
+    Shader(const GLenum, const std::string&);
     ~Shader();
 
-    // The source Shader points to nothing after copy/move
-    Shader(Shader&);
+    Shader(const Shader&);
+    Shader& operator=(const Shader&);
+
     Shader(Shader&&);
-    Shader& operator=(Shader&);
     Shader& operator=(Shader&&);
 
     void Attach(const GLuint) const;
     void Detach(const GLuint) const;
 
   private:
+    void Compile(const std::string&) const;
+
     GLuint id_;
   };
 }
