2333d8375a4dd2002098b6a6537abb9900d04839
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-05-31 14:50:42 -0400

    Increase BuildString.cmake readability

--- BuildString.cmake
+++ BuildString.cmake
@@ -1,3 +1,10 @@
-file(READ ${CMAKE_ARGV1}/${CMAKE_ARGV3} STRING)
+set(DELIMITER "")
+
+set(SOURCE ${CMAKE_ARGV1})
+set(BUILD ${CMAKE_ARGV2})
+set(PATH ${CMAKE_ARGV3})
+
+file(READ ${SOURCE}/${PATH} STRING)
+# add semicolons back into string
 string(REGEX REPLACE ";" "\\\\;" STRING "${STRING}")
-file(WRITE ${CMAKE_ARGV2}/${CMAKE_ARGV3}.txt R\"\(${STRING}\)\")
+file(WRITE ${BUILD}/${PATH}.txt R\"${DELIMITER}\(${STRING}\)${DELIMITER}\")
