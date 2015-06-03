a05a634780a7fbcdc8b1d10a29d22ea1d9676492
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-06-03 09:59:28 -0400

    Use switch statements for key events

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -43,39 +43,61 @@ void Game::CursorPosCallback(const double xpos, const double ypos) {
 
 void Game::KeyCallback(const int key, const int scancode, const int action, const int mods) {
   if (action == GLFW_PRESS) {
-    if (key == GLFW_KEY_ESCAPE) {
-      glfwSetInputMode(
-        window_,
-        GLFW_CURSOR,
-        glfwGetInputMode(window_, GLFW_CURSOR) == GLFW_CURSOR_NORMAL ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL
+    switch (key) {
+      case GLFW_KEY_ESCAPE:
+        glfwSetInputMode(
+          window_,
+          GLFW_CURSOR,
+          glfwGetInputMode(window_, GLFW_CURSOR) == GLFW_CURSOR_NORMAL ? GLFW_CURSOR_DISABLED : GLFW_CURSOR_NORMAL
         );
-    }
-    if (key == GLFW_KEY_LEFT_CONTROL) {
-      world_.SetCameraDeltaVertical(-10.0F);
-    } else if (key == GLFW_KEY_SPACE) {
-      world_.SetCameraDeltaVertical(10.0F);
-    } else if (key == GLFW_KEY_W) {
-      world_.SetCameraDeltaForward(10.0F);
-    } else if (key == GLFW_KEY_S) {
-      world_.SetCameraDeltaForward(-10.0F);
-    } else if (key == GLFW_KEY_A) {
-      world_.SetCameraDeltaStrafe(-10.0F);
-    } else if (key == GLFW_KEY_D) {
-      world_.SetCameraDeltaStrafe(10.0F);
-    } else if (key == GLFW_KEY_LEFT_BRACKET) {
-      world_.SetCameraDeltaRoll(1.0F);
-    } else if (key == GLFW_KEY_RIGHT_BRACKET) {
-      world_.SetCameraDeltaRoll(-1.0F);
+        break;
+      case GLFW_KEY_LEFT_CONTROL:
+        world_.SetCameraDeltaVertical(-10.0F);
+        break;
+      case GLFW_KEY_SPACE:
+        world_.SetCameraDeltaVertical(10.0F);
+        break;
+      case GLFW_KEY_W:
+        world_.SetCameraDeltaForward(10.0F);
+        break;
+      case GLFW_KEY_S:
+        world_.SetCameraDeltaForward(-10.0F);
+        break;
+      case GLFW_KEY_A:
+        world_.SetCameraDeltaStrafe(-10.0F);
+        break;
+      case GLFW_KEY_D:
+        world_.SetCameraDeltaStrafe(10.0F);
+        break;
+      case GLFW_KEY_LEFT_BRACKET:
+        world_.SetCameraDeltaRoll(1.0F);
+        break;
+      case GLFW_KEY_RIGHT_BRACKET:
+        world_.SetCameraDeltaRoll(-1.0F);
+        break;
+      default:
+        break;
     }
   } else if (action == GLFW_RELEASE) {
-    if (key == GLFW_KEY_LEFT_CONTROL || key == GLFW_KEY_SPACE) {
-      world_.SetCameraDeltaVertical(0.0F);
-    } else if (key == GLFW_KEY_W || key == GLFW_KEY_S) {
-      world_.SetCameraDeltaForward(0.0F);
-    } else if (key == GLFW_KEY_A || key == GLFW_KEY_D) {
-      world_.SetCameraDeltaStrafe(0.0F);
-    } else if (key == GLFW_KEY_LEFT_BRACKET || key == GLFW_KEY_RIGHT_BRACKET) {
-      world_.SetCameraDeltaRoll(0.0F);
+    switch (key) {
+      case GLFW_KEY_LEFT_CONTROL:
+      case GLFW_KEY_SPACE:
+        world_.SetCameraDeltaVertical(0.0F);
+        break;
+      case GLFW_KEY_W:
+      case GLFW_KEY_S:
+        world_.SetCameraDeltaForward(0.0F);
+        break;
+      case GLFW_KEY_A:
+      case GLFW_KEY_D:
+        world_.SetCameraDeltaStrafe(0.0F);
+        break;
+      case GLFW_KEY_LEFT_BRACKET:
+      case GLFW_KEY_RIGHT_BRACKET:
+        world_.SetCameraDeltaRoll(0.0F);
+        break;
+      default:
+        break;
     }
   }
 }
