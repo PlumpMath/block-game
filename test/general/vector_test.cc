577a85fbc5f8d01b78a51c1cd4b93b8ea487c790
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-06-05 14:25:24 -0400

    Use success parameter in tests instead of global data

--- test/general/vector_test.cc
+++ test/general/vector_test.cc
@@ -4,9 +4,7 @@
 #include "general/vector.h"
 
 namespace {
-bool success{true};
-
-void TestDefaultConstructor() {
+void TestDefaultConstructor(bool& success) {
   block_game::Vector<10> vector;
   float expected[10]{0.0F};
   for (size_t i{0}; i < 10; ++i) {
@@ -18,7 +16,7 @@ void TestDefaultConstructor() {
   }
 }
 
-void TestInitializerListConstructor() {
+void TestInitializerListConstructor(bool& success) {
   block_game::Vector<10> vector{1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 9.0F, 10.0F};
   float expected[]{1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 9.0F, 10.0F};
   for (size_t i{0}; i < 10; ++i) {
@@ -32,7 +30,8 @@ void TestInitializerListConstructor() {
 }
 
 int main() {
-  TestDefaultConstructor();
-  TestInitializerListConstructor();
+  bool success{true};
+  TestDefaultConstructor(success);
+  TestInitializerListConstructor(success);
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
 }
