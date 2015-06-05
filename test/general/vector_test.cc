3aa86b576bd755e8963d16277dd3512a7cae6ee5
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-06-05 19:29:31 -0400

    Refactor whitespace in vector_test.cc

--- test/general/vector_test.cc
+++ test/general/vector_test.cc
@@ -10,6 +10,7 @@ namespace {
 void TestDefaultConstructor(bool& success) {
   block_game::Vector<10> vector;
   float expected[10]{0.0F};
+
   for (size_t i{0}; i < 10; ++i) {
     if (vector[i] != expected[i]) {
       std::cerr << "default constructor failed: ";
@@ -22,6 +23,7 @@ void TestDefaultConstructor(bool& success) {
 void TestInitializerListConstructor(bool& success) {
   block_game::Vector<10> vector{1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 9.0F, 10.0F};
   float expected[]{1.0F, 2.0F, 3.0F, 4.0F, 5.0F, 6.0F, 7.0F, 8.0F, 9.0F, 10.0F};
+
   for (size_t i{0}; i < 10; ++i) {
     if (vector[i] != expected[i]) {
       std::cerr << "initializer list constructor failed: ";
