6f6486ba32a02a7437a8edfc097880baf318f154
RedRoboHood <RedRoboHood@users.noreply.github.com>
2015-06-13 09:47:12 -0400

    Add Matrix default constructor test

--- test/general/matrix_test.cc
+++ test/general/matrix_test.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iostream>
 
 #include <lightest/lightest.h>
 
@@ -9,9 +10,33 @@ using lightest::Update;
 namespace {
 const size_t kTestOrder{3};
 typedef block_game::Matrix<kTestOrder> TestMatrix;
+
+bool TestDefaultConstructor() {
+  const TestMatrix matrix;
+  const float expected[kTestOrder][kTestOrder]{
+    {1.0F, 0.0F, 0.0F},
+    {0.0F, 1.0F, 0.0F},
+    {0.0F, 0.0F, 1.0F}
+  };
+
+  bool success{true};
+  for (size_t i{0}; i < kTestOrder; ++i) {
+    for (size_t j{0}; j < kTestOrder; ++j) {
+      if (matrix[i][j] != expected[i][j]) {
+        std::cerr << "default constructor failed: ";
+        std::cerr << "element [" << i << "][" << j << "] is " << matrix[i][j] << " instead of " << expected[i][j] << std::endl;
+        success = false;
+      }
+    }
+  }
+  return success;
+}
 }
 
 int main() {
   bool success{true};
+
+  Update(success, TestDefaultConstructor());
+
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
 }
